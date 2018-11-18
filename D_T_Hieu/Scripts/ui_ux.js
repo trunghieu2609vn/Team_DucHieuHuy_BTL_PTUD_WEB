4ad7f13cc03f5af41aea1b3ca78130931719c1ba
= <33550236+trunghieu2609vn@users.noreply.github.com>
2018-11-19 00:11:37 +0700

    update 0.5

---
+++ D_T_Hieu/Scripts/ui_ux.js
@@ -0,0 +1,72 @@
+$('.btn-create').click(function() {
+	$('.create-subject').fadeToggle(500);
+});
+$('.delete-subject').click(function() {
+	var idSub = $(this).parent().siblings('.id-subject').text();
+	var id = idSub.toUpperCase();
+	swal({
+	  	title: "Bạn có chắc chắn muốn xóa?",
+	  	text: "Khi bạn xác nhận, khóa học "+ id+ " sẽ KHÔNG CÒN TỒN TẠI ở bất cứ đâu trên hệ thống!",
+	  	icon: "warning",
+	  	buttons: true,
+	  	dangerMode: true,
+	})
+	.then((willDelete) => {
+	  	if (willDelete) {
+	    	swal("Bạn đã xóa thành công khóa học!", {
+	      	icon: "success",
+	    	});
+	  	} else {
+	    	swal("Khóa học chưa bị xóa!");
+	  	}
+	});
+});
+//sửa khóa học,tính năng tạm thời,sẽ dùng json đổ dữ liệu sau
+$('#btn-edit').click(function() {
+	window.open("../BTL_PTUD_WEB/adminPageView.html");
+});
+
+// Tắt gửi form nếu xảy ra trường hợp chưa được điền
+(function checkCreate() {
+  	'use strict';
+  	window.addEventListener('load', function() {
+
+	    // lấy tất cả các input trong form
+	    var forms = document.getElementsByClassName('needs-validation');
+	    // lặp và ngăn k được gửi nếu chưa được điền
+	    var validation = Array.prototype.filter.call(forms, function(form) {
+	      	form.addEventListener('submit', function(event) {
+		        if (form.checkValidity() === false) {
+		          	event.preventDefault();
+		          	event.stopPropagation();
+		        }
+	        	form.classList.add('was-validated'); 
+	      	}, false);
+	    });
+  	}, false);
+  	
+})();
+
+$('.btn-create-course').click(function() {
+	
+	swal({
+	  title: "Bạn có chắc chắn muốn thêm khóa học?",
+	  icon: "warning",
+	  buttons: true,
+	  dangerMode: true,
+	})
+	.then((willDelete) => {
+	  if (willDelete) {
+	    swal("Bạn đã thêm thành công khóa học!", {
+	      icon: "success",
+	    });
+	  } else {
+	    swal("Khóa học chưa được thêm!");
+	  }
+	});
+});
+
+$('.div-check').children('.checked, .unchecked').click(function () {
+    $(this).toggleClass('checked');
+    $(this).toggleClass('unchecked');
+});
